808a8d5d0e175dd33d8c99b6123633fb24c8178c
Matej Baco <matejbacocom@gmail.com>
2021-08-26 11:31:51 +0200

    Formatted the code

--- app/controllers/posts.php
+++ app/controllers/posts.php
@@ -12,74 +12,72 @@ App::get('/posts')
     ->inject('response')
     ->inject("db")
     ->action(
-        function($request, $response, $database) {
+        function ($request, $response, $database) {
             /** @var  Utopia\Database\Database $database */
-           $posts = $database->find("posts");
+            $posts = $database->find("posts");
 
             $response
-              ->json(['posts' => $posts]);
+                ->json(['posts' => $posts]);
         }
     );
 
-    App::get('/posts/:postId')
+App::get('/posts/:postId')
     ->param('postId', '', new Text(255))
     ->inject('request')
     ->inject('response')
     ->inject("db")
     ->action(
-        function($postId, $request, $response, $database) {
-            /** @var  Utopia\Database\Database $database */
-
-           $post = $database->getDocument("posts", $postId);
-
-           if($post->isEmpty()) {
-              return $response->setStatusCode(404)->json([ "error" => "Post not found." ]);
-           }
-
-            $response
-              ->json(['post' => $post]);
-        }
-    );
-
-    App::post('/posts')
-    ->param('author', '', new Text(255))
-    ->param('title', '', new Text(255))
-    ->param('text', '', new Text(16777216))
-        ->inject('request')
-        ->inject('response')
-        ->inject("db")
-       
-        ->action(
-            function($author, $title, $text, $request, $response, $database) {
-                /** @var  Utopia\Database\Database $database */
-    
-               $newPost = $database->createDocument("posts", new Document([
-                   "title" => $title,
-                   "author" => $author,
-                   "text" => $text,
-                   '$write' => ["role:all"],
-                   '$read' => ["role:all"],
-               ]));
-    
-                $response
-                  ->json(['post' => $newPost]);
-            }
-        );
-
-App::delete('/posts')
-->param('id', '', new Text(255))
-    ->inject('request')
-    ->inject('response')
-    ->inject("db")
-    
-    ->action(
-        function($postId, $request, $response, $database) {
+        function ($postId, $request, $response, $database) {
             /** @var  Utopia\Database\Database $database */
 
             $post = $database->getDocument("posts", $postId);
 
-            if($post->isEmpty()) {
-                return $response->setStatusCode(404)->json([ "error" => "Post not found." ]);
+            if ($post->isEmpty()) {
+                return $response->setStatusCode(404)->json(["error" => "Post not found."]);
+            }
+
+            $response
+                ->json(['post' => $post]);
+        }
+    );
+
+App::post('/posts')
+    ->param('author', '', new Text(255))
+    ->param('title', '', new Text(255))
+    ->param('text', '', new Text(16777216))
+    ->inject('request')
+    ->inject('response')
+    ->inject("db")
+    ->action(
+        function ($author, $title, $text, $request, $response, $database) {
+            /** @var  Utopia\Database\Database $database */
+
+            $newPost = $database->createDocument("posts", new Document([
+                "title" => $title,
+                "author" => $author,
+                "text" => $text,
+                '$write' => ["role:all"],
+                '$read' => ["role:all"],
+            ]));
+
+            $response
+                ->json(['post' => $newPost]);
+        }
+    );
+
+App::delete('/posts')
+    ->param('id', '', new Text(255))
+    ->inject('request')
+    ->inject('response')
+    ->inject("db")
+    ->action(
+        function ($postId, $request, $response, $database) {
+            /** @var  Utopia\Database\Database $database */
+
+            $post = $database->getDocument("posts", $postId);
+
+            if ($post->isEmpty()) {
+                return $response->setStatusCode(404)->json(["error" => "Post not found."]);
             }
 
             $success = $database->deleteDocument("posts", $postId);
@@ -98,21 +96,21 @@ App::patch('/posts')
     ->inject('response')
     ->inject("db")
     ->action(
-        function($postId, $title, $text, $request, $response, $database) {
+        function ($postId, $title, $text, $request, $response, $database) {
             /** @var  Utopia\Database\Database $database */
 
             $post = $database->getDocument("posts", $postId);
 
-            if($post->isEmpty()) {
-                return $response->setStatusCode(404)->json([ "error" => "Post not found." ]);
+            if ($post->isEmpty()) {
+                return $response->setStatusCode(404)->json(["error" => "Post not found."]);
             }
 
             $post->setAttribute("title", $title);
             $post->setAttribute("text", $text);
 
-           $success = $database->updateDocument("posts", $postId, $post);
+            $success = $database->updateDocument("posts", $postId, $post);
 
             $response
-              ->json(['post' => $post]);
+                ->json(['post' => $post]);
         }
     );
